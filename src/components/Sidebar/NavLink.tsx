5534601f2e83184dfa96abdaebab5842d9b952b7
João Rodrigo Prieto <jrp2033@gmail.com>
2022-02-17 22:28:44 -0300

    Side link active

--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -1,6 +1,7 @@
 import { Icon, Link as Chakralink, Text, LinkProps } from "@chakra-ui/react";
 import { ElementType } from "react";
 import Link from 'next/link'
+import { ActiveLink } from "../ActiveLink";
 
 interface NavLinkprops extends LinkProps {
     icon: ElementType;
@@ -10,11 +11,11 @@ interface NavLinkprops extends LinkProps {
 
 export function NavLink({ icon, children, href, ...rest }: NavLinkprops) {
     return (
-        <Link href={href} passHref>
+        <ActiveLink href={href} passHref>
             <Chakralink display="flex" align="center" {...rest}>
                 <Icon as={icon} fontSize="20" />
                 <Text ml="4" fontWeight="medium">{children}</Text>
             </Chakralink>
-        </Link>
+        </ActiveLink>
     );
 }
